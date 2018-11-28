0d70701e50bc1056ad5da858bd7675ff3a020e05
zabtimofei <zab.timofei@yandex.ru>
2018-11-28 14:27:48 +0300

    Выполнил домашние задания № 1-4 2 урока

--- HomeWork2/HomeWork2.1/Program.cs
+++ HomeWork2/HomeWork2.1/Program.cs
@@ -10,13 +10,29 @@ namespace HomeWork2._1
     {
         static void Main(string[] args)
         {
-            double x = 5;
-            double z = 6;
-            double y = 6;
-            double max = MaxFrom3(x, y, z);
-            Console.WriteLine("max=" + NumberOfDigits(834554.2345326));
+           bool aut =  Autorization("root", "GeekBrains");
+            if (aut == false) return;
+            // С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+            Console.WriteLine("\n"+"Начинайте вводить число");
+            int a;
+            int sum = 0;
+            char b;
+            do
+            {
+                b = Console.ReadKey().KeyChar;
+                a = int.Parse(b.ToString());
+                if (a > 0 && (a % 2) != 0)
+                {
+                    sum += a;
+                }
+            }
+            while (b != '0');
+            Console.WriteLine("\nСумма нечетных положтельных чисел равна " + sum);
             Console.ReadLine();
+
+
         }
+
         /// <summary>Возвращает максимальное из трех чисел</summary>
 
         private static double MaxFrom3(double x, double y, double z)
@@ -31,12 +47,43 @@ namespace HomeWork2._1
         {
             string a = x.ToString();
             int i = 0;
-            foreach(char c in a)
+            foreach (char c in a)
             {
                 if (c == ',' || c == '.') continue;
                 else i++;
             }
             return i;
+        }
+
+        private static bool Autorization(string login, string password)
+        {
+            bool a;
+            int i = 0;
+            do { 
+            Console.WriteLine("Введите логин: ");
+            string perslogin = Console.ReadLine();
+            Console.WriteLine("Введите пароль: ");
+            string persPass = Console.ReadLine();
+            
+
+                if (perslogin == login && persPass == password)
+                {
+                    a = true;
+                    Console.WriteLine("Авторизация прошла успешно");
+              
+                    return a;
+                }
+                else
+                {
+                    a = false;
+                    if (i < 3) Console.WriteLine("Введен неверный логин и/или пароль. Повторите попытку");
+                    else Console.WriteLine("Неверный логин и пароль. До свидания");
+                    i++;
+                    
+                 }
+            }
+            while (i < 3);
+            return a;
         }
     }
 }
