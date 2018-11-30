11b7093c032e53c685e2b72436955cf23d81c4ca
zabtimofei <zab.timofei@yandex.ru>
2018-11-30 10:35:36 +0300

    нача

--- HomeWork3/HomeWork3/Program.cs
+++ HomeWork3/HomeWork3/Program.cs
@@ -8,16 +8,50 @@ namespace HomeWork3
 {
     class Program
     {
-        struct Complex
-        {
-            double _Re;
-            double _Im;
 
-            double 
-
-        }
         static void Main(string[] args)
         {
         }
     }
+    struct Complex
+    {
+        public double _Re;
+        public double _Im;
+
+        public Complex(double Re, double Im)
+        {
+            _Re = Re;
+            _Im = Im;
+        }
+        public Complex Plus(Complex x)
+        {
+            Complex y;
+            y._Re=_Re + x._Re;
+            y._Im =_Im+ x._Im;
+            return y;
+        }
+
+        public Complex Multi (Complex x)
+        {
+            Complex y;
+            y._Re = _Re * x._Re - _Im * x._Im;
+            y._Im = _Re * x._Im + _Im * x._Re;
+            return y;
+        }
+
+        public Complex Minus (Complex x)
+        {
+            Complex y;
+            y._Re = _Re - x._Re;
+            y._Im = _Im - x._Im;
+            return y;
+        }
+
+
+
+    }
+
+
+
 }
+
